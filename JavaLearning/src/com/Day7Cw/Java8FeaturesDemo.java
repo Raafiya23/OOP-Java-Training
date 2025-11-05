f22f002437cb90a23704ada9c007f31615dc08e3
Raafiya23 <raafiyasultana03@gmail.com>
2025-11-05 10:28:17 +0530

    feat: added Day7Cw file

---
+++ JavaLearning/src/com/Day7Cw/Java8FeaturesDemo.java
@@ -0,0 +1,20 @@
+package com.Day7Cw;
+
+import java.time.format.SignStyle;
+
+interface Operation{
+    int execute(int a, int b);
+}
+
+public class Java8FeaturesDemo {
+    public static void main(String[] args){
+        Operation add = (a,b) -> a+b;
+        System.out.println(add.execute(10,20));
+
+        Operation sub = (a,b) -> a-b;
+        System.out.println(sub.execute(45,12));
+
+        Operation mul = (a, b) -> a*b;
+        System.out.println(mul.execute(12,5));
+    }
+}
