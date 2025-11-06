4b93cda36caecda09993072fa59f7f0438286b31
Raafiya23 <raafiyasultana03@gmail.com>
2025-11-06 09:30:07 +0530

    feat: added RemoveDuplicate program

---
+++ JavaLearning/src/com/Day6Cw/RemoveDuplicates.java
@@ -0,0 +1,15 @@
+package com.Day6Cw;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RemoveDuplicates {
+    public static void main(String[] args){
+        String[] fruits={"Apple","Orange","Banana","Apple","Orange","Banana","Pineapple"};
+        Set<String> hs=new HashSet<>();
+        for(String temp:fruits){
+            hs.add(temp);
+        }
+        System.out.println(hs);
+    }
+}
